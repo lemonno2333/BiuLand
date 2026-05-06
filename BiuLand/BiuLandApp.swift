539079c1578f9e0feadaa86fabbc9caca5f93577
lemonno2333 <w1173751525@gmail.com>
2026-05-06 14:07:32 +0800

    Initial Commit

---
+++ BiuLand/BiuLandApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiuLandApp.swift
+//  BiuLand
+//
+//  Created by Leo on 06/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct BiuLandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
